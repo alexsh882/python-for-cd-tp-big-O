7b258037f58a8e688286f98dc8c38a001ea5aa8d
alexsh882 <alexsh882@gmail.com>
2024-06-07 10:29:09 -0300

    feat: Add function to add numeric elements of an array

--- sum_numeric_array_elements.py
+++ sum_numeric_array_elements.py
@@ -7,3 +7,10 @@ def sum_numeric_array_numbers(arr):
         if type(element) == int or type(element) == float:
             sum += element
     return sum
+
+def sum_numeric_array_numbers_n(n):
+    """
+    Función que retorna la suma de los elementos numéricos de un arreglo.
+    """
+    arr = [i for i in range(n)]
+    return sum_numeric_array_numbers(arr)
