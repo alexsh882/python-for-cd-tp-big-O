17a97d73a9d914a9c3e1c68ca0f4ab427c5f2d4c
alexsh882 <alexsh882@gmail.com>
2024-06-07 10:28:06 -0300

    chore: Add first_element_array_n function to retrieve the first element of an array of size n

--- first_element_array.py
+++ first_element_array.py
@@ -3,3 +3,10 @@ def first_element_array(arr):
     Función que retorna el primer elemento de un arreglo.
     """
     return arr[0]
+
+def first_element_array_n(n):
+    """
+    Función que retorna el primer elemento de un arreglo.
+    """
+    arr = [i for i in range(n)]
+    return first_element_array(arr)
