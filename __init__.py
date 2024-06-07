8be0f820b09e4d6815506712f641ea0e3c018df3
alexsh882 <alexsh882@gmail.com>
2024-06-07 10:29:53 -0300

    feat: Add benchmarking functionality and main function.

--- __init__.py
+++ __init__.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 from bubble_sort import bubble_sort_n
 from binary_search import binary_search_n
 from benchmark import benchmark
-from lineal_search import lineal_search
-from quick_sort import quicksort
-from first_element_array import first_element_array
-from sum_numeric_array_elements import sum_numeric_array_numbers
+from quick_sort import quicksort_n
+from first_element_array import first_element_array_n
+from sum_numeric_array_elements import sum_numeric_array_numbers_n
+from fibonacci_sequence import fibonacci_sequence
 
-N = 100;
+# Cantidad de veces que se ejecutará la función.
+N = 20
 
 
+# Función que mide el tiempo de ejecución de la función dada como argumento.
 def o_test(func):
     """
     Función que mide el tiempo de ejecución de la función.
@@ -21,22 +23,31 @@ def o_test(func):
     return results
 
 
-def graph(results):
+def graph(results, label):
     """
-    Función que grafica los resultados de la función.
+    Función que gráfica los resultados de la función.
     """
-    plt.plot(results)
-    plt.show()
+    plt.plot(results, label=label)
+    plt.xlabel("n")
+    plt.ylabel("tiempo")
+    plt.title("Trabajo Práctico sobre Notación Big O - Python")
+    plt.legend()
+    return plt
+
 
 def main():
     """
     Función principal el benchmarking de las funciones.
     """
-    # Se ejecuta la función de búsqueda binaria.
-    print('binary', o_test(binary_search_n))
-    print('bubble', o_test(bubble_sort_n))
+    # Gráfica de las funciones.
+    pt = graph(o_test(first_element_array_n), "O(1) - Constante")
+    pt = graph(o_test(binary_search_n), "O(log n) - Logarítmica")
+    pt = graph(o_test(sum_numeric_array_numbers_n), "O(n) - Lineal")
+    pt = graph(o_test(quicksort_n), "O(n log n) - Linealítmica")
+    pt = graph(o_test(bubble_sort_n), "O(n^2) - Cuadrática")
+    pt = graph(o_test(fibonacci_sequence), "O(2^n) - Exponencial")
+    pt.show()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
